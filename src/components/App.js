01af093465d54024cbed4cdb5f645828e36bd690
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-12-23 15:52:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
-
+import { useState } from "react";
+import styles from "./Greetings.module.css";
 import React from "react";
-import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState();
+
+  const displayChange = (e) => {
+    setName(e.target.value);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+       <div className={styles.form}>
+        <label className={styles.label} htmlFor="name">Enter Your name:</label>
+        {/* <br /> */}
+        <input
+          className={styles.input}
+          name="name"
+          value={name}
+          type="text"
+          onChange={displayChange}
+        />
+      </div>
+
+      <div className={styles.output}>
+        {name && <h2>Hello {name}!</h2>}
+        </div>
     </div>
   )
 }
